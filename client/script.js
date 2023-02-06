bb8485640b57daedb21a67c116bbb9f6ee1f24ea
Ben (Minds) <bbe0391@gmail.com>
2023-02-05 23:45:02 -0600

    minor update

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
 //fetch data from server
-const response = await fetch('https://firstai.onrender.com', {
+const response = await fetch('https://firstai.onrender.com/', {
 	method: 'POST',
 	headers: {
 			'Content-Type': 'application/json'
